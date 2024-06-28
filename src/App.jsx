76b690c772fea2544b6b390902e781392dfd57d8
Opeyemi Rahael-Ogbe <scienceshephard@gmail.com>
2024-06-28 12:44:16 +0100

    Routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { Routes, Route } from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import './App.css'
 import InfoPage from './Contents/info'
-import ErrorPage from './ErrorPage'
 import Layout from './Layout'
 import Plan from './Contents/plan'
 import AddsComponent from './Contents/adds'
 import SummaryComponent from './Contents/summary'
 import ThankYou from './Contents/ThankYouPage'
+
 function App() {
-  
-  return (
-    <>
-      <div className="container">
-        <Layout />
-          <Routes>
-            <Route path='/' element={<InfoPage />} />
-            <Route path='*' element={<ErrorPage />} />
-            <Route path='/plan' element={<Plan/>} />
-            <Route path='/plan/adds' element={ <AddsComponent/> } />
-            <Route path='/plan/adds/summary' element={<SummaryComponent />} />
-            <Route path='/thankyou'element={ <ThankYou /> } />
-          </Routes>
-      </div>
-    </>
-  )
+
+    return (
+        <>
+            <div className="container">
+                <Layout/>
+                <Routes>
+                    <Route path='*' element={<InfoPage/>}/>
+                    {/*<Route path='*' element={<ErrorPage/>}/>*/}
+                    <Route path='/plan' element={<Plan/>}/>
+                    <Route path='/plan/adds' element={<AddsComponent/>}/>
+                    <Route path='/plan/adds/summary' element={<SummaryComponent/>}/>
+                    <Route path='/thankyou' element={<ThankYou/>}/>
+                </Routes>
+            </div>
+        </>
+    )
 }
 
 export default App
